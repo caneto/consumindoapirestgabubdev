e95f9e896fe0b7af348e6bdd823247d1073f4145
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2022-11-28 18:22:33 -0300

    Update user.dart

---
+++ examples/user.dart
@@ -0,0 +1,8 @@
+const json = '''{
+  "id": "USDSF",
+  "name": "Loren Loren",
+  "profileUri": "http://image",
+  "createAt": "2022-01-01-19:00", //DATETIME
+  "email":"email@gmail.com",
+  "bio":"Flutter Developer"
+}''';
